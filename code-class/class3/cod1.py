d3da144d9a7fce5e111c5bd1096a3fd1eaf05350
Mauricio Neto <maumneto@gmail.com>
2020-07-27 15:30:25 -0300

    nwe code

--- code-class/class3/cod1.py
+++ code-class/class3/cod1.py
@@ -1,1 +1,43 @@
+import math
+
 # codigo aula 3
+idade = 29 # tipo inteiro
+nota = 7.6 # tipo real
+nome = 'Mauricio' # tipo literal
+estaVago = True # tipo booleano
+
+print(type(idade))
+print(type(nota))
+print(type(nome))
+print(type(estaVago))
+
+# Este é um comentário de linha!
+
+"""
+Comentários 
+em 
+Multiplas
+linhas
+"""
+'''
+Comentários 
+em 
+Multiplas
+linhas
+'''
+# idade = int(input('Entre com a idade: '))
+# print(type(idade))
+# nota = float(input('Entre com a nota do aluno: '))
+# print(type(nota))
+
+# nome = input('Entre com o seu nome: ')
+# print('Olá Sr(a) ',nome)
+
+valor = 4
+print(math.sqrt(valor))
+pi = 3.1415
+print(math.ceil(pi))
+print(math.floor(pi))
+print(round(pi,2))
+# print(dir(math))
+help(math.ceil)
---
+++ code-class/class3/cod2.py
@@ -0,0 +1,11 @@
+nome = input('Entre com o nome: ')
+matricula = int(input('Entre com a matrícula: '))
+curso = input('Entre com o seu curso: ')
+idade = int(input('Entre com a sua idade: '))
+email = input('Digite seu email: ')
+
+print('Nome: ', nome)
+print('matrícula: ', matricula)
+print('Curso: ', curso)
+print('Idade: ', idade)
+print('Email: ', email)
