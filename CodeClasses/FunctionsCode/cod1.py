bcc222449e0be965ab59deff5b7473bd5fd29cdc
Mauricio Neto <maumneto@gmail.com>
2020-09-17 15:16:35 -0300

    adjustments

--- CodeClasses/FunctionsCode/cod1.py
+++ CodeClasses/FunctionsCode/cod1.py
@@ -1,7 +1,6 @@
+# O módulo Math possui um conjunto de funções
 import math
 
 num1 = int(input('Digite um valor numérico: '))
-
-res = math.sqrt(num1)
-
+res = math.sqrt(num1) # nesta linha o código principal irá parar para executar a função math no sistema de módulos do python!
 print(f'A raiz quadrada de {num1} é {res}')
